b9b4b3a11f0dd900e1e0f0f7563bcc695ebdb4fc
Mark Russinovich <mark@Marks-MacBook-Pro.local>
2017-04-10 16:47:49 -0700

    Added home.html

--- geodjango/geodjango/settings.py
+++ geodjango/geodjango/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
+    'corsheaders',
     'heatmap',
 ]
 
